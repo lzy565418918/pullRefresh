5f905180eaa7b5019a2a8b285a5cc893d4792ef4
liuziyang <gnayizuil@gmail.com>
2019-12-30 15:53:31 +0800

    'vant上拉加载和下拉刷新'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  img{
+    width: 80px;
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,161 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="list">
+    <div class="header">
+      <span>图标</span>
+      <span>名称</span>
+      <span>技能</span>
+    </div>
+    <van-pull-refresh
+      v-model="isRefresh"
+      @refresh="onRefresh"
+      success-text="刷新成功"
+      :error.sync="errRefresh"
+    >
+      <van-list
+        v-model="isLoading"
+        :finished="isFinished"
+        finished-text="我是有底线的~"
+        @load="onLoad"
+        :error.sync="isError"
+        :offset="offset"
+      >
+        <div class="content" v-for="(item,index) in list" :key="index">
+          <img class="heroIcon" :src="item.icon" :alt="item.name" />
+          <span class="heroName">{{item.name}}</span>
+          <span class="heroSkill">{{item.skill}}</span>
+        </div>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
-
 <script>
+import { List, PullRefresh } from "vant";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    [List.name]: List,
+    [PullRefresh.name]: PullRefresh
+  },
+  data() {
+    return {
+      // 是否正在加载中
+      isLoading: false,
+      // 是否加载已完成
+      isFinished: false,
+      // 是否加载出错
+      isError: false,
+      // 数据源
+      list: [],
+      // 当前页
+      currentPage: 1,
+      // 一页有多少条
+      pageSize: 150,
+      // 总页数
+      totalPage: 1,
+      // 滚动条距离底部多少像素触发加载更多
+      offset: 100,
+      // 是否正处于刷新状态
+      isRefresh: false,
+      // 刷新是否失败
+      errRefresh: false
+    };
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    // 加载更多
+    onLoad() {
+      // 如果当前页大于总页数，则加载已完成
+      if (this.currentPage > this.totalPage) {
+        this.isFinished = true;
+        return;
+      }
+      // 无网络直接请求失败
+      if (!navigator.onLine) {
+        this.isError = true;
+        return
+      }
+      this.$axios
+        .get("https://autumnfish.cn/api/cq/page", {
+          params: {
+            pageNum: this.currentPage,
+            pageSize: this.pageSize
+          }
+        })
+        .then(res => {
+          // 记录总页数
+          this.totalPage = res.data.totalPage;
+          // 累加数据
+          this.list = [...this.list, ...res.data.list];
+          this.currentPage++;
+          // 需要置为false才能继续加载
+          this.isLoading = false;
+          console.log(this.list);
+        })
+        .catch(() => {
+          this.isError = true;
+        });
+    },
+    onRefresh() {
+      if (!navigator.onLine) {
+        this.errRefresh = true;
+        return;
+      }
+      // 初始化当前页数
+      this.currentPage = 1;
+      this.$axios
+        .get("https://autumnfish.cn/api/cq/page", {
+          params: {
+            pageNum: this.currentPage,
+            pageSize: this.pageSize
+          }
+        })
+        .then(res => {
+          //  记录总页数
+          this.totalPage = res.data.totalPage;
+          // 只获取第一页数据
+          this.list = res.data.list;
+          this.currentPage++;
+          this.isRefresh = false;
+          console.log(this.list);
+        });
+    }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="less" scoped>
+#list {
+  .header {
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid #aaaaaa;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 16px;
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    span {
+      display: inline-block;
+      font-size: 18px;
+      color: red;
+      height: 40px;
+      line-height: 40px;
+    }
+    .heroIcon {
+      width: 30px;
+      height: 30px;
+      vertical-align: center;
+    }
+  }
 }
 </style>
